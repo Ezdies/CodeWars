6f0f0e5484d2570aab9bdb9eea42f87af778adb3
Ezdies <bvbclubbvb@gmail.com>
2023-08-13 16:16:15 +0200

    another python kata

--- python/Kata5/whatIsPP.py
+++ python/Kata5/whatIsPP.py
@@ -1,14 +1,10 @@
-import math
-
-def isPP(n):
-    max_exponent = int(math.log(n, 2)) + 1
-    for base in range(2, int(math.sqrt(n)) + 1):
-        exponent = 2
-        while base ** exponent <= n and exponent <= max_exponent:
-            if base ** exponent == n:
-                return [base, exponent]
-            exponent += 1
-    return None
+def josephus_survivor(n, k):
+    circle = list(range(1, n + 1))
+    index = 0
+    while len(circle) > 1:
+        index = (index + k - 1) % len(circle)
+        circle.pop(index)
+    return circle[0] if circle else None
 
             
         
